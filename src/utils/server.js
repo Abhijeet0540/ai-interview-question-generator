68c6b2622a2963be51226573e1fc41c6410943cc
abhi d <abhijeetd439@gmail.com>
2025-05-20 12:05:13 +0530

    update json package

--- src/utils/server.js
+++ src/utils/server.js
@@ -1,6 +1,6 @@
-import app from "./src/app.js";
+import app from "../app.js";
 import dotenv from 'dotenv';
-import "./src/config/db.postgres.config.js";
+import "../config/db.postgres.config.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
